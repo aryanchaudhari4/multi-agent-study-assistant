16fb887058498083dc8d2783a2569c385e05e9cb
aryanchaudhari4 <ac7345437@gmail.com>
2026-09-02 09:52:50 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     gemini_api_key: str
     database_url: str = "sqlite:///./studyassistant.db"
-    secret_key: str = "mysecretkey123"
+    secret_key: str
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 10080
     model: str = "gemini-2.0-flash-lite"
